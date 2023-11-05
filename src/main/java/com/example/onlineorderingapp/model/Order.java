2b4c458ca6373d137c2f18001a89cbc5c6f205e4
godaxeles <axeles687@gmail.com>
2023-11-06 02:28:41 +0500

    add Order Model #4

---
+++ src/main/java/com/example/onlineorderingapp/model/Order.java
@@ -0,0 +1,11 @@
+package com.example.onlineorderingapp.model;
+
+public class Order {
+        private Long id;
+        private String product;
+        private String status;
+        private String destination;
+        private Long requestId;
+        private String userName;
+}
+
