9d1f1890dd83207a6aaf968e0a11da8adb040753
godaxeles <axeles687@gmail.com>
2023-11-10 01:22:34 +0500

    add: OrderService #6

---
+++ src/main/java/com/online/ordering/app/service/OrderService.java
@@ -0,0 +1,58 @@
+package com.online.ordering.app.service;
+
+import com.online.ordering.app.model.OrderModel;
+import com.online.ordering.app.model.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<OrderModel> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<OrderModel> getOrderById(Long orderId) {
+        return orderRepository.findById(orderId);
+    }
+
+    public OrderModel createOrder(OrderModel order) {
+        // Потом при надобности добавлю логику при создании если нужно
+        return orderRepository.save(order);
+    }
+
+    public OrderModel updateOrder(Long orderId, OrderModel updatedOrder) {
+        Optional<OrderModel> existingOrderOptional = orderRepository.findById(orderId);
+
+        if (existingOrderOptional.isPresent()) {
+            OrderModel existingOrder = existingOrderOptional.get();
+
+            existingOrder = updatedOrder;
+
+            return orderRepository.save(existingOrder);
+        } else {
+            // Обработка, если заказ не найден
+            return null;
+        }
+    }
+
+    public boolean cancelOrder(Long orderId) {
+        Optional<OrderModel> existingOrderOptional = orderRepository.findById(orderId);
+
+        if (existingOrderOptional.isPresent()) {
+            orderRepository.deleteById(orderId);
+            //Сделал удаление, если нужно просто статус менять чтобы он не отображался то сделаю так.
+            return true; // Успешно отменено
+        } else {
+            return false; // Заказ не найден
+        }
+    }
+}
