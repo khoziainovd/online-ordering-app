ee5b6349add8a0747838fab763837e14378a64ec
godaxeles <axeles687@gmail.com>
2023-11-13 19:24:28 +0500

    edit: direction #6

--- src/main/java/com/online/ordering/app/repository/OrderRepository.java
+++ src/main/java/com/online/ordering/app/repository/OrderRepository.java
@@ -1,5 +1,6 @@
-package com.online.ordering.app.model;
+package com.online.ordering.app.repository;
 
+import com.online.ordering.app.model.OrderModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<OrderModel, Long> {
